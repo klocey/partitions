4b269070ca58764db613a01852e890fa0916c92c
Kenneth John Locey <ken@weecology.org>
2013-04-26 12:08:19 -0600

    edited comments -KL

--- misc_part_func.py
+++ misc_part_func.py
@@ -10,7 +10,7 @@ import random, decimal
 
 """ Functions for integer partitioning. Most apply to using integer partitioning to examine distributions
     of wealth and abundance using the feasible set. The feasible set is the set all forms of the distribution
-    having the same constraint values (e.g. total abundance N, species richness S). However, most mathematical
+    having the same constraint values (e.g. total abundance N, species richness S). Most mathematical
     environment (all that I know of) do not have functions 3 through 10.
 
 Included functions:
