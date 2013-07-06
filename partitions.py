954a0daed6bf8e540ebb27fd7f23a8ba619fe4ce
Dan McGlinn <danmcglinn@gmail.com>
2013-07-05 20:43:35 -0600

    Set default for method to best on rand_parts()

--- partitions.py
+++ partitions.py
@@ -110,7 +110,7 @@ def NrParts(*arg):
         parts = p[Q - N + 1]
     return parts
 
-def rand_parts(Q, N, sample_size, method, D={}, zeros=False):
+def rand_parts(Q, N, sample_size, method='best', D={}, zeros=False):
     """
     Generate uniform random partitions of Q having N parts.
     
@@ -120,7 +120,7 @@ def rand_parts(Q, N, sample_size, method, D={}, zeros=False):
         sample_size : number of random partitions to generate
         method : method to use for generating the partition, options include:
             'bottom_up', 'top_down', 'divide_and_conquer', 'multiplicity', and
-            'best'
+            'best'. Defaults to 'best'
         D : a dictionary for the number of partitions of Q having N or less
             parts (or N or less as the largest part), i.e. P(Q, Q + N). Defaults
             to a blank dictionary.
