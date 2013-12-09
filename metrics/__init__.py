c05ab2df5a6aeac006e62fd3f7f384620111b359
Dan McGlinn <dancmglinn@gmail.com>
2013-12-09 14:29:47 -0700

    Add init files for pypartitions and metrics

---
+++ metrics/__init__.py
@@ -0,0 +1,9 @@
+"""
+metrics: statistical metrics for vectors of integers
+
+Reference: Locey, K.J. and D.J. McGlinn. Efficient algorithms for sampling feasible sets
+of macroecological patterns. PeerJ. https://peerj.com/preprints/78/
+
+"""
+
+from .metrics import *
---
+++ pypartitions/__init__.py
@@ -0,0 +1,10 @@
+"""
+pypartitions: Efficient Sampling Algorithims for Integer Partitioning
+
+Reference: Locey, K.J. and D.J. McGlinn. Efficient algorithms for sampling feasible sets
+of macroecological patterns. PeerJ. https://peerj.com/preprints/78/
+
+"""
+
+
+from .pypartitions import *
