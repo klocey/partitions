b7f8ae2ec7997365495cb8aa110aa2d8e7cea6df
Dan McGlinn <dancmglinn@gmail.com>
2013-12-09 20:51:19 -0700

    Replace argument use_dict with use_hash so that it is clearer that the possible data storage unit is a hashtable.  Revise annotations for the function P().

--- rpartitions/R/rpartitions.R
+++ rpartitions/R/rpartitions.R
@@ -10,6 +10,7 @@ get_rand_int = function(min=0, max=1) {
   return(int)
 }
 
+
 #' Returns the last element of a vector
 #' 
 #' @param x a vector
@@ -18,6 +19,7 @@ get_rand_int = function(min=0, max=1) {
 #' last(1:10)
 #' last(letters[1:10])
 last = function(x) { tail(x, n = 1) }
+
 
 #' Find the conjugate of an integer partition
 #' Recoded (orginally on 24-Apr-2013) from the Sage source code:
@@ -50,6 +52,7 @@ conjugate = function(partition, use_c=TRUE){
   }
   return(conj)
 }
+
 
 #' Find the number of partitions for a given total Q and number of parts N.
 #' Recoded and modified from GAP source code: www.gap-system.org.  
@@ -119,17 +122,33 @@ NrParts = function(Q, N=NULL, use_c=TRUE){
   return(numparts)
 }
 
-#' Number of partitions of q with k or less parts (or having k or less as the
-#' largest part), i.e. P(q+k,k).
+
+#' Number of partitions of q with k or less parts.
 #' 
-#' @param D lookup table for numbers of partitions, P(q+k,k) values. 
-#' @param q total sum of the set
+#'     Note:
+#'     1. Theorem: The number of partitions of q with k or less parts equals the
+#'     number of partitions of q with k or less as the largest part (see Bona 2006).
+#'     This is a mathematical symmetry, i.e. congruency.
+#'     Bona, M. (2006). A Walk Through Combinatorics: An Introduction to Enumeration
+#'     and Graph Theory. 2nd Ed. World Scientific Publishing Co. Singapore.
+#'     
+#'     2. Proposition: The number of partitions of q with k or less parts equals
+#'     the number of partitions of q+k with k as the largest part when k>0,
+#'     i.e. P(q + k, k). No source known, but it can be shown when enumerating the
+#'     entire feasible set or using Sage:
+#'     
+#' @param D lookup table for numbers of partitions of q having k or less parts 
+#' (or k or less as the largest part), i.e. P(q, q + k)
+#' @param q total (i.e., sum across all k or n parts)
+#' @param k the number of parts and also the size of the largest part (congruency)
 #' @param use_c logical, if TRUE the number of partitions is computed in c
+#' @param use_hash logical, if TRUE then a hash table is used instead of R's native
+#' list to store the information
 #' @export
 #' @examples
 #' P(list(), 100, 10, FALSE, FALSE)
-P = function(D, q, k, use_c, use_dict) {
-  if (use_dict) {
+P = function(D, q, k, use_c, use_hash) {
+  if (use_hash) {
     key = paste(q, k, sep=',')
     if (!has.key(key, D)) {
       D[key] = NrParts(q + k, k, use_c)
@@ -158,7 +177,7 @@ P = function(D, q, k, use_c, use_dict) {
 #' @param zeros : boolean if True partitions can have zero values, if False
 #'        partitions have only positive values, defaults to False
 #' @param use_c : boolean if TRUE then compiled c code is used, defaults to TRUE
-#' @param use_dict : boolean, if TRUE then a hash table is used, defaults to FALSE
+#' @param use_hash : boolean, if TRUE then a hash table is used, defaults to FALSE
 #' @return A matrix where each column is a random partition
 #' @note method == 'best' attempts to use the values of Q and N to infer what the 
 #'         fastest method to compute the partition.
@@ -166,13 +185,13 @@ P = function(D, q, k, use_c, use_dict) {
 #' @examples
 #' rand_parts(100, 10, 5)
 rand_partitions = function(Q, N, sample_size, method='best', D=hash(), zeros=FALSE,
-                      use_c=TRUE, use_dict=FALSE) {
+                      use_c=TRUE, use_hash=FALSE) {
   parts = matrix(NA, ncol=sample_size, nrow=N)
   if (zeros) {
-    Plist = P(D, Q, N, use_c, use_dict)
+    Plist = P(D, Q, N, use_c, use_hash)
   }  
   else {
-    Plist = P(D, Q - N, N, use_c, use_dict)
+    Plist = P(D, Q - N, N, use_c, use_hash)
   }  
   D = Plist[[1]]
   numparts = Plist[[2]]
@@ -188,22 +207,22 @@ rand_partitions = function(Q, N, sample_size, method='best', D=hash(), zeros=FAL
       part = N
     }  
     if (method == 'bottom_up') {
-      part = bottom_up(part, q, D, rand_int, use_c, use_dict)
+      part = bottom_up(part, q, D, rand_int, use_c, use_hash)
     }  
     if (method == 'top_down') {
-      part = top_down(part, q, D, rand_int, use_c, use_dict)
+      part = top_down(part, q, D, rand_int, use_c, use_hash)
     }  
     if (method == 'divide_and_conquer') {
-      part = divide_and_conquer(part, q, N, D, rand_int, use_c, use_dict)
+      part = divide_and_conquer(part, q, N, D, rand_int, use_c, use_hash)
     }  
     if (method == 'multiplicity') {
-      part = multiplicity(part, q, D, rand_int, use_c, use_dict)
+      part = multiplicity(part, q, D, rand_int, use_c, use_hash)
     }  
     if (method == 'best') { 
       if (Q < 250 | N >= Q / 1.5)
-        part = bottom_up(part, q, D, rand_int, use_c, use_dict)
+        part = bottom_up(part, q, D, rand_int, use_c, use_hash)
       else
-        part = divide_and_conquer(part, q, N, D, rand_int, use_c, use_dict)
+        part = divide_and_conquer(part, q, N, D, rand_int, use_c, use_hash)
     }  
     if (zeros) {
       Zs = rep(0, N - length(part))
@@ -224,18 +243,18 @@ rand_partitions = function(Q, N, sample_size, method='best', D=hash(), zeros=FAL
 #'   parts (or N or less as the largest part), i.e. P(Q, Q + N).        
 #' @param rand_int the random integer
 #' @param use_c boolean if TRUE then compiled c code is used
-#' @param use_dict boolean, if TRUE then hash dictionary is used
+#' @param use_hash boolean, if TRUE then hash dictionary is used
 #' @export
 #' @examples
 #' bottom_up(c(5, 4), 4, hash(), 1, TRUE, FALSE)
-bottom_up = function(part, q, D, rand_int, use_c, use_dict) {
+bottom_up = function(part, q, D, rand_int, use_c, use_hash) {
   while (q > 0) {
     for (k in 1:q) {
-      Plist = P(D, q, k, use_c, use_dict)
+      Plist = P(D, q, k, use_c, use_hash)
       D = Plist[[1]]
       count = Plist[[2]]
       if (count >= rand_int) {
-        Plist = P(D, q, k - 1, use_c, use_dict)
+        Plist = P(D, q, k - 1, use_c, use_hash)
         D = Plist[[1]]
         count = Plist[[2]]
         break
@@ -261,11 +280,11 @@ bottom_up = function(part, q, D, rand_int, use_c, use_dict) {
 #'   parts (or N or less as the largest part), i.e. P(Q, Q + N).        
 #' @param rand_int the random integer
 #' @param use_c boolean if TRUE then compiled c code is used
-#' @param use_dict boolean, if TRUE then hash dictionary is used
+#' @param use_hash boolean, if TRUE then hash dictionary is used
 #' @export
 #' @examples
 #' top_down(c(5, 4), 4, hash(), 1, TRUE, FALSE)
-top_down = function(part, q, D, rand_int, use_c, use_dict) {
+top_down = function(part, q, D, rand_int, use_c, use_hash) {
   while (q > 0) {
     if (!is.null(part)) {
       x = min(part)
@@ -274,7 +293,7 @@ top_down = function(part, q, D, rand_int, use_c, use_dict) {
       x = q
     }  
     for (k in x:1) {
-      Plist = P(D, q, k, use_c, use_dict) # number of partitions of q having k or less as the largest part
+      Plist = P(D, q, k, use_c, use_hash) # number of partitions of q having k or less as the largest part
       D = Plist[[1]]
       count = Plist[[2]]
       if (count < rand_int) {
@@ -301,19 +320,19 @@ top_down = function(part, q, D, rand_int, use_c, use_dict) {
 #'        parts (or N or less as the largest part), i.e. P(Q, Q + N).        
 #' @param rand_int the random integer
 #' @param use_c boolean if TRUE then compiled c code is used
-#' @param use_dict boolean, if TRUE then hash dictionary is used
+#' @param use_hash boolean, if TRUE then hash dictionary is used
 #' @export
 #' @examples
 #' divide_and_conquer(c(5, 4), 5, 4, hash(), 2, TRUE, FALSE)
-divide_and_conquer = function(part, q, N, D, rand_int, use_c, use_dict) {
+divide_and_conquer = function(part, q, N, D, rand_int, use_c, use_hash) {
   max_int = N
   min_int = 1 
   while (q > 0) {
     k = get_rand_int(min_int, max_int)
-    Plist = P(D, q, k, use_c, use_dict)
+    Plist = P(D, q, k, use_c, use_hash)
     D = Plist[[1]]
     upper = Plist[[2]]
-    Plist = P(D, q, k - 1, use_c, use_dict)
+    Plist = P(D, q, k - 1, use_c, use_hash)
     D = Plist[[1]]
     lower = Plist[[2]]
     if (lower < rand_int & rand_int <= upper) {
@@ -346,15 +365,15 @@ divide_and_conquer = function(part, q, N, D, rand_int, use_c, use_dict) {
 #' @param rand_int the random integer
 #' @param count count < rand_int
 #' @param use_c boolean if TRUE then compiled c code is used
-#' @param use_dict boolean, if TRUE then hash dictionary is used
+#' @param use_hash boolean, if TRUE then hash dictionary is used
 #' @export
 #' @examples
 #' get_multiplicity(10, 5, hash(), 3, 2, TRUE, FALSE)
-get_multiplicity = function(q, k, D, rand_int, count, use_c, use_dict){
+get_multiplicity = function(q, k, D, rand_int, count, use_c, use_hash){
   multi = NULL # the multiplicity 
   f = 1
   while (f > 0) {
-    Plist = P(D, (q - k * f), k - 1, use_c, use_dict)
+    Plist = P(D, (q - k * f), k - 1, use_c, use_hash)
     D = Plist[[1]]
     count = count + Plist[[2]]
     if (count >= rand_int) {
@@ -380,7 +399,7 @@ get_multiplicity = function(q, k, D, rand_int, count, use_c, use_dict){
 #' @export
 #' @examples
 #' multiplicity(c(5, 4), 4, hash(), 1, TRUE, FALSE) 
-multiplicity =  function(part, q, D, rand_int, use_c, use_dict){
+multiplicity =  function(part, q, D, rand_int, use_c, use_hash){
   while (q > 0) {
     multi = NULL
     if (!is.null(part)) {
@@ -390,7 +409,7 @@ multiplicity =  function(part, q, D, rand_int, use_c, use_dict){
       x = q
     }
     for (k in x:1) { # start with largest k
-      Plist = P(D, q, k, use_c, use_dict) # number of partitions of q having k or less as the largest part
+      Plist = P(D, q, k, use_c, use_hash) # number of partitions of q having k or less as the largest part
       D = Plist[[1]]
       count = Plist[[2]]
       if (count == rand_int & rand_int == 1) {
